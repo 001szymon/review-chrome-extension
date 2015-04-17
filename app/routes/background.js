d7cfc18756b0c18dfb52987580ebc010fa50b94d
Krzysztof Urban <krzysztof.urban@netguru.pl>
2015-04-17 18:00:22 +0200

    message response stub

--- app/routes/background.js
+++ app/routes/background.js
@@ -2,10 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   background: Ember.inject.service(),
+  commitLookup: Ember.inject.service(),
   model () {
     return this.store.find('project');
   },
   afterModel (projects) {
+    this.setupListener();
     this.get('background').run(projects);
+  },
+  setupListener () {
+    if (!chrome)
+      return;
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+      console.log(request);
+      sendResponse({ status: "accepted" });
+    });
   }
 });
