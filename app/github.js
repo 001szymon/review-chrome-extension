bdc85a6399f9e16f38c86b3c7db8c3980df92cf8
Wiktor Schmidt <wiktorschmidt@gmail.com>
2015-04-17 18:04:49 +0200

    buttons - styles look like shit

--- app/github.js
+++ app/github.js
@@ -1,8 +1,19 @@
-$('.timeline-comment .form-actions button.btn-primary')
-  .before("<button class='btn btn-danger'><span class='octicon octicon-x' aria-hidden='true'></span> Rejected</button>")
-  .before("<button class='btn btn-success'><span class='octicon octicon-check' aria-hidden='true'></span> Accepted</button>")
+//$('.timeline-comment .form-actions button.btn-primary')
+//  .before("<button class='btn btn-danger'><span class='octicon octicon-x' aria-hidden='true'></span> Rejected</button>")
+//  .before("<button class='btn btn-success'><span class='octicon octicon-check' aria-hidden='true'></span> Accepted</button>")
 
 commit = window.location.href.split("/")[6];
 project = window.location.href.split("/")[4];
 
-chrome.runtime.sendMessage({commit: commit, project: project}, function(r) { console.log(r); console.log(chrome.runtime.lastError); });
+chrome.runtime.sendMessage({commit: commit, project: project}, function(r) {
+  but = $('.timeline-comment .form-actions button.btn-primary');
+  if (r && r.state == "accepted") {
+    but.before("<div><span class='octicon octicon-thumbsup' aria-hidden='true'></span> Accepted</div>")
+  } else if (r && r.state == "rejected") {
+    but.before("<div><span class='octicon octicon-x' aria-hidden='true'></span> Rejected</div>")
+  } else {
+    but.before("<div><span class='octicon octicon-sync' aria-hidden='true'></span> Pending</div>")
+  };
+  console.log(r);
+  console.log(chrome.runtime.lastError);
+});
