f84657a230c35f74f03d676314ea4fb47adc0981
Wiktor Schmidt <wiktorschmidt@gmail.com>
2015-04-17 18:05:32 +0200

    buttons - styles look like shit still

--- app/github.js
+++ app/github.js
@@ -8,11 +8,11 @@ project = window.location.href.split("/")[4];
 chrome.runtime.sendMessage({commit: commit, project: project}, function(r) {
   but = $('.timeline-comment .form-actions button.btn-primary');
   if (r && r.state == "accepted") {
-    but.before("<div><span class='octicon octicon-thumbsup' aria-hidden='true'></span> Accepted</div>")
+    but.before("<div><span class='octicon octicon-thumbsup' aria-hidden='true'></span> Accepted</div>");
   } else if (r && r.state == "rejected") {
-    but.before("<div><span class='octicon octicon-x' aria-hidden='true'></span> Rejected</div>")
+    but.before("<div><span class='octicon octicon-x' aria-hidden='true'></span> Rejected</div>");
   } else {
-    but.before("<div><span class='octicon octicon-sync' aria-hidden='true'></span> Pending</div>")
+    but.before("<div class='btn-group' role='group'><button class='btn btn-link'><span class='octicon octicon-sync' aria-hidden='true'></span> Pending</button><button class='btn btn-danger'><span class='octicon octicon-x' aria-hidden='true'></span> Reject</button><button class='btn btn-success'><span class='octicon octicon-check' aria-hidden='true'></span> Accept</button></div>");
   };
   console.log(r);
   console.log(chrome.runtime.lastError);
